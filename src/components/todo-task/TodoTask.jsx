89680d242355b8b2aaaf23cd281bfcd74fc9a9f9
Calet Devia <calebtius@gmail.com>
2025-06-06 10:07:05 -0500

    Added the function to delete the tasks

--- src/components/todo-task/TodoTask.jsx
+++ src/components/todo-task/TodoTask.jsx
@@ -1,13 +1,50 @@
 const TodoTask = ({ taskName, taskList, setTaskList }) => {
+  const handleCheckbox = (e) => {
+    const checkValue = e.target.checked;
+
+    /* Find the same the same task name in the array  */
+
+    setTaskList((prevTaskList) => {
+      const findIndex = prevTaskList.find(({ name }) => name === taskName);
+
+      console.log(findIndex);
+
+      return [
+        ...prevTaskList.map((task) => {
+          const { name, state } = task;
+          if (findIndex === task) {
+            return { name, state: checkValue };
+          }
+          return task;
+        }),
+      ];
+    });
+  };
+
+  const handleDeleteBtn = () => {
+    console.log(taskName);
+    console.log();
+
+    console.log(taskList);
+    setTaskList((prevTaskList) => {
+      return prevTaskList.filter(({ name }) => name !== taskName);
+    });
+  };
+
   return (
     <>
       <div>
-        <input type="checkbox" name="checkTask" id="checkTask" />
+        <input
+          onClick={handleCheckbox}
+          type="checkbox"
+          name="checkTask"
+          id="checkTask"
+        />
         <div>
           <strong>{taskName}</strong>
           <div>
             <button>Edit</button>
-            <button>Delete</button>
+            <button onClick={handleDeleteBtn}>Delete</button>
           </div>
         </div>
       </div>
